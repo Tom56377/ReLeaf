cdbef7c56c1a4df07d2574cc8ab9066ec4d5ef26
Tom Buttwell <tb16213@essex.ac.uk>
2020-10-14 15:42:58 +0100

    update

--- src/BlockeyMain.java
+++ src/BlockeyMain.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.concurrent.Flow;
 
 public class BlockeyMain {
 
@@ -8,30 +9,47 @@ public class BlockeyMain {
         JFrame frame = new JFrame("Blockey");  //create frame
         frame.getContentPane().setPreferredSize(new Dimension(500,350));
 
-        FlowLayout layout = new FlowLayout(FlowLayout.CENTER,40,40); //add flow layout for buttons
 
-        /*blockey message positioning*/
-        JPanel panelBlockeyMessage = new JPanel(); //add panel for buttons and add to frame
-        panelBlockeyMessage.setLayout(layout);
-        frame.add(panelBlockeyMessage);
 
-        /*user input positioning*/
-        JPanel panelUserInput = new JPanel(); //add panel for buttons and add to frame
-        panelBlockeyMessage.setLayout(layout);
-        frame.add(panelUserInput);
-
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        frame.add(panel);
 
         /*blockey message label*/
-        JLabel blockeyMessage = new JLabel("hello, how can I help you today?"); //blockey default message
+        JLabel blockeyMessage = new JLabel("Hello, how can I help you today?",SwingConstants.CENTER); //blockey default message
         blockeyMessage.setPreferredSize(new Dimension(200,100));
         blockeyMessage.setFont(new Font("Arial", Font.BOLD, 12));
-        panelBlockeyMessage.add(blockeyMessage);
 
+        c.gridx = 1;
+        c.gridy = 0;
+
+        panel.add(blockeyMessage,c);
+
+
+
+
+
+        /*userIput text area*/
+        JScrollPane scroll = new JScrollPane ();
+        scroll.setVerticalScrollBarPolicy(scroll.VERTICAL_SCROLLBAR_ALWAYS);
 
         JTextArea userInput = new JTextArea();
-        userInput.setPreferredSize(new Dimension(200,100));
-        blockeyMessage.setFont(new Font("Arial", Font.BOLD, 12));
-        panelUserInput.add(userInput);
+        userInput.setFont(new Font("Arial", Font.BOLD, 12));
+        userInput.setLineWrap(true);
+
+        c.gridx = 1;
+        c.gridy = 3;
+
+        scroll.getViewport().add(userInput);
+        scroll.setPreferredSize(new Dimension(200,100));
+
+        panel.add(scroll,c);
+
+
+
+
+
+
 
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
